c3bcc0bf71c622bd1c2b0fe3627f20173136ebc8
NwlSmith <44346148+NwlSmith@users.noreply.github.com>
2019-02-14 13:25:37 -0500

    Added Tristen's Workspace

---
+++ Tristen's workspace/Assets/Scripts/Coin.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Coin : MonoBehaviour
+{
+
+    // variable to store value of point when coin is collected
+    public int pointValue = 1;
+
+    // this triggers when the sprite collides with enything else that has a ridgid body
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        // pt2 check to see if player has collided with the coin
+        if (collision.tag == "Player")
+        {
+            // no score yet, it should disappear when collided with player
+            // well... this should only be called if the player touches it
+
+            // v1 destory game object when the player collides with it
+            // Destroy(gameObject);
+
+
+            // v2 add sounds
+
+            // Destroy(gameObject);
+            // GetComponent<AudioSource>().Play();
+
+            // did it work? No because it is destoryed before the sound can play
+            // reorder
+
+            // GetComponent<AudioSource>().Play();
+            // Destroy(gameObject);
+
+            // did it work? No because even though the sound plays first, the sprite is destoryed too quickly 
+            // add delay to destory
+
+            // GetComponent<AudioSource>().Play();
+            // Destroy(gameObject,4f); // long delay for teaching purposes
+
+            // does it work now? Yes, the sound plays and then the sprite is destoryed
+            // how could we make it better? - hide sprite as soon as the playr collides with it
+
+
+            // final coin sound script
+            GetComponent<SpriteRenderer>().enabled = false;
+            GetComponent<AudioSource>().Play();
+            Destroy(gameObject,0.2f); // long delay for teaching purposes
+
+
+        }
+    }
+
+
+    
+        
+    
+
+
+}
---
+++ Tristen's workspace/Assets/Scripts/Player.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    // Global Variable
+    // go over what a variable is
+    public bool alive = true;
+
+    // this represents the rigidbody of our sprite
+    // it has things like velocity which we can modify when we move our player
+    private Rigidbody2D rb2D;
+
+    // variable to store horizontal speed of player
+    public float horizontalSpeed = 10.0f;
+
+    // variable to store vertical speed of the player
+    public float jumpSpeed = 10.0f;
+    
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // get and store rigid body from sprite so we can change it
+        rb2D = GetComponent<Rigidbody2D>();
+        
+    }
+
+    // Update is called once per pysics frame
+    void FixedUpdate()
+    {
+        // if the player is currently alive..
+        if(alive == true)
+        {
+            // get input from the keyboard keys and store in float variable
+            // ad left right arrows
+            float moveHorizontal = Input.GetAxis("Horizontal");
+            // ws up down arrows
+            float moveVertical = Input.GetAxis("Vertical");
+
+            // will store the current velocity of the player
+            Vector2 targetVelocity = rb2D.velocity;
+
+            // need to update left and right velocity
+            // check to see if the player is pressing someting in the left or right axis!
+            if(moveHorizontal != 0)
+            {
+                // need to change/update the player x velocity 
+                targetVelocity.x = moveHorizontal * horizontalSpeed;
+            }
+
+            // check if the player is pressing just the up arrow or W
+            if (moveVertical > 0 && rb2D.velocity.y == 0)
+            {
+                //set the velocity up and down of the player to a #
+                targetVelocity.y = jumpSpeed;
+            }
+
+
+
+            // update the velocity of the player to our new velocity based on keyboard input 
+            rb2D.velocity = targetVelocity;
+        }
+
+        
+
+    }
+}
