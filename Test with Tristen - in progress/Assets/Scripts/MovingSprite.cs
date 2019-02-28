6f522e4bd2b0878198cbf09d10240e64408f6de6
MaxT2 <Tristen@payne.org>
2019-02-28 13:18:37 -0500

    Fixed MovingPlatform Merge Conflict

--- Test with Tristen - in progress/Assets/Scripts/MovingSprite.cs
+++ Test with Tristen - in progress/Assets/Scripts/MovingSprite.cs
@@ -4,7 +4,6 @@ using UnityEngine;
 
 public class MovingSprite : MonoBehaviour
 {
-<<<<<<< HEAD
     // store the boundaries of the platform
     public float distanceFromStart = 2f;
 
@@ -13,16 +12,6 @@ public class MovingSprite : MonoBehaviour
 
     // the speed at which the sprite will move
     public float speed = 3f;
-=======
-    // store the left and right boundaries of the platform
-    // the sprite will not move beyond these bounds
-    public float leftBoundary = 2f;
-    public float rightBoundary = 1f;
-
-    // keep track of if the sprite is moving right or left
-    // this can be set in the inspector for each gameobject
-    public bool movingLeft = true;
->>>>>>> 1766178d58df8570c0f84735a74eec9425bef479
 
     // store the start position of the vector
     private Vector2 startPosition;
@@ -45,7 +34,6 @@ public class MovingSprite : MonoBehaviour
         // if movingRight == true, ie, if movingRight != false, ie, if moving right
         if (movingRight == true)
         {
-<<<<<<< HEAD
             //move new position to the right by speed
             newPosition.x = transform.position.x + speed * Time.fixedDeltaTime;
             // if the newPosition.x is to the right of distanceFromStart, ie, out of bounds
@@ -54,15 +42,10 @@ public class MovingSprite : MonoBehaviour
                 // move left
                 movingRight = false;
             }
-=======
-            // move new position to the left by taking x component of current position, and moving it to the left a certain distance.
-            newPosition.x = transform.position.x - 1f * Time.fixedDeltaTime;
->>>>>>> 1766178d58df8570c0f84735a74eec9425bef479
         }
-        // if the platform is moving left expand here
+        // if the platform is moving left - expand here
         else
         {
-<<<<<<< HEAD
             // move new position to the left by speed
             newPosition.x = transform.position.x - speed * Time.fixedDeltaTime;
             // if the newPosition.x is to the left of distanceFromStart, ie, out of bounds
@@ -71,10 +54,6 @@ public class MovingSprite : MonoBehaviour
                 // make it move right
                 movingRight = true;
             }
-=======
-            //move new position to the right by taking x component of current position, and moving it to the right a certain distance.
-            newPosition.x = transform.position.x + 1f * Time.fixedDeltaTime;
->>>>>>> 1766178d58df8570c0f84735a74eec9425bef479
         }
 
         //use the rigidbody2d to set the current position to the updated new position
