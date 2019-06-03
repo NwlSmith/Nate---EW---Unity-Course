61a9e810bcd62d9827a8cb4de4706eebf7869162
Nate Smith <me@nwlsmith.com>
2019-06-03 15:40:25 -0400

    Added FixedCameraRotationADDON, fixed final Player Script

--- Finished Project/Assets/Scripts/Player.cs
+++ Finished Project/Assets/Scripts/Player.cs
@@ -18,6 +18,9 @@ public class Player : MonoBehaviour
     // we can modify these values to move our player
     private Rigidbody2D rb2D;
 
+    // boolean that stores whether or not the player is alive
+    public bool alive = true;
+
     private void Start()
     {
         // get and store the rigid body 2d from this game object so we can change it
@@ -28,66 +31,55 @@ public class Player : MonoBehaviour
     private void FixedUpdate()
     {
         // if the player is still alive, allow input
-
-        // get input from the keyboard keys and store in float variable from -1 to 1
-        // -1 for left, +1 for right
-        // when there is no input, this value will go back to 0.
-        // A and D OR left and right arrows
-        float moveHorizontal = Input.GetAxis("Horizontal");
-        // gets input from the keyboard keys Up and Down OR W and S
-        // only W is used.
-        // -1 for S, +1 for W
-        float moveVertical = Input.GetAxis("Vertical");
-
-        // from the RigidBody2D, which stores variables regarding movement and physics
-        // store the current velocity of the player so we can modify it
-        // and later set the current velocity to this new modified target
-        Vector2 targetVelocity = rb2D.velocity;
-
-        // if the keyboard input gave us a value that is NOT 0,
-        // as in, if a button was pressed on the horizontal axis
-        if (moveVertical > 0 && rb2D.velocity.y == 0)
+        if (alive)
         {
-            // get the audiosource component from the gameobject, and play the current clip.
-            GetComponent<AudioSource>().Play();
+            // get input from the keyboard keys and store in float variable from -1 to 1
+            // -1 for left, +1 for right
+            // when there is no input, this value will go back to 0.
+            // A and D OR left and right arrows
+            float moveHorizontal = Input.GetAxis("Horizontal");
+            // gets input from the keyboard keys Up and Down OR W and S
+            // only W is used.
+            // -1 for S, +1 for W
+            float moveVertical = Input.GetAxis("Vertical");
 
-            // set the x component of the target velocity to the keyboard input directions (from -1 to 1)
-            // multiplied by the movement speed
-            targetVelocity.y = m_jumpSpeed;
+            // from the RigidBody2D, which stores variables regarding movement and physics
+            // store the current velocity of the player so we can modify it
+            // and later set the current velocity to this new modified target
+            Vector2 targetVelocity = rb2D.velocity;
+
+            // if the keyboard input gave us a value that is NOT 0,
+            // as in, if a button was pressed on the horizontal axis
+            if (moveVertical > 0 && rb2D.velocity.y == 0)
+            {
+                // get the audiosource component from the gameobject, and play the current clip.
+                GetComponent<AudioSource>().Play();
+
+                // set the x component of the target velocity to the keyboard input directions (from -1 to 1)
+                // multiplied by the movement speed
+                targetVelocity.y = m_jumpSpeed;
+            }
+
+            // if the player is pressing the jump button, moveVertical will go to +1, so if the player presses jump
+            // and they are not either already jumping or falling
+            if (moveHorizontal != 0)
+                // set the vertical target velocity to the jump speed, which will make the object shoot upward
+                targetVelocity.x = moveHorizontal * m_horizontalSpeed;
+
+            // Finally, from the rigidbody, set the objects velocity to the new target velocity.
+            rb2D.velocity = targetVelocity;
         }
-
-        // if the player is pressing the jump button, moveVertical will go to +1, so if the player presses jump
-        // and they are not either already jumping or falling
-        if (moveHorizontal != 0)
-            // set the vertical target velocity to the jump speed, which will make the object shoot upward
-            targetVelocity.x = moveHorizontal * m_horizontalSpeed;
-
-        // Finally, from the rigidbody, set the objects velocity to the new target velocity.
-        rb2D.velocity = targetVelocity;
 
     }
 
     // called when the player starts to collide with another object
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        // if the collision was with a platform object
-        if (collision.gameObject.tag == "Platform")
+        // if the collision was with an enemy object
+        if (collision.gameObject.tag == "Enemy")
         {
-            // parent the player to that platform.
-            // this is to have the player move with the platform, because children move with their parents.
-            //transform.parent = collision.transform;
-        }
-    }
-
-
-    // called when the player stops colliding with another object
-    private void OnCollisionExit2D(Collision2D collision)
-    {
-        // if the collision was with a platform object
-        if (collision.gameObject.tag == "Platform")
-        {
-            // unparent the player from the platform. This will allow the player to move freely
-            transform.parent = null;
+            // set player to dead
+            alive = false;
         }
     }
 
---
+++ Initial Project/Assets/FINISHED ADDONS/Scripts/FixedCameraRotationADDON.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FixedCameraRotationADDON : MonoBehaviour
+{
+    private void FixedUpdate()
+    {
+        transform.eulerAngles = Vector3.zero;
+    }
+}
