aba02bdf9db1dfad845112667cc07d3fab25cf36
Nate Smith <me@nwlsmith.com>
2019-06-03 15:49:19 -0400

    Changed horizontal to x etc.

--- Finished Project/Assets/Scripts/Player.cs
+++ Finished Project/Assets/Scripts/Player.cs
@@ -5,10 +5,10 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
 
-    // variable to store horizontal speed of player
+    // variable to store x speed of player
     // because it is public, you will be able to change it from the inspector.
     // the player will move this many units per second.
-    public float m_horizontalSpeed = 10f;
+    public float m_xSpeed = 10f;
     // variable to store vertical speed of the player
     // the player will have their vertical velocity set to this value when jumping
     public float m_jumpSpeed = 10f;
@@ -37,20 +37,31 @@ public class Player : MonoBehaviour
             // -1 for left, +1 for right
             // when there is no input, this value will go back to 0.
             // A and D OR left and right arrows
-            float moveHorizontal = Input.GetAxis("Horizontal");
+            float moveX = Input.GetAxis("X");
             // gets input from the keyboard keys Up and Down OR W and S
             // only W is used.
             // -1 for S, +1 for W
-            float moveVertical = Input.GetAxis("Vertical");
+            float moveY = Input.GetAxis("Y");
 
             // from the RigidBody2D, which stores variables regarding movement and physics
             // store the current velocity of the player so we can modify it
             // and later set the current velocity to this new modified target
             Vector2 targetVelocity = rb2D.velocity;
 
+
+
             // if the keyboard input gave us a value that is NOT 0,
-            // as in, if a button was pressed on the horizontal axis
-            if (moveVertical > 0 && rb2D.velocity.y == 0)
+            // as in, if a button was pressed on the X axis
+            if (moveX != 0)
+            {
+                // set the vertical target velocity to the jump speed, which will make the object shoot upward
+                targetVelocity.x = moveX * m_xSpeed;
+            }
+
+            
+            // if the player is pressing the jump button, moveVertical will go to +1, so if the player presses jump
+            // and they are not either already jumping or falling
+            if (moveY > 0 && rb2D.velocity.y == 0)
             {
                 // get the audiosource component from the gameobject, and play the current clip.
                 GetComponent<AudioSource>().Play();
@@ -59,12 +70,6 @@ public class Player : MonoBehaviour
                 // multiplied by the movement speed
                 targetVelocity.y = m_jumpSpeed;
             }
-
-            // if the player is pressing the jump button, moveVertical will go to +1, so if the player presses jump
-            // and they are not either already jumping or falling
-            if (moveHorizontal != 0)
-                // set the vertical target velocity to the jump speed, which will make the object shoot upward
-                targetVelocity.x = moveHorizontal * m_horizontalSpeed;
 
             // Finally, from the rigidbody, set the objects velocity to the new target velocity.
             rb2D.velocity = targetVelocity;
